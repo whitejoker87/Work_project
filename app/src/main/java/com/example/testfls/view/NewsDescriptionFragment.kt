242ea5e01345a2307be7d724aebbf7d436bcdef8
AOrekhov <alexey.orekhov@firstlinesoftware.com>
2019-11-20 12:16:30 +0300

    databinding newsdscrfragment

--- app/src/main/java/com/example/testfls/view/NewsDescriptionFragment.kt
+++ app/src/main/java/com/example/testfls/view/NewsDescriptionFragment.kt
@@ -2,9 +2,12 @@ package com.example.testfls.view
 
 import android.os.Bundle
 import android.view.*
+import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.testfls.R
+import com.example.testfls.databinding.FragmentNewsDescriptionBinding
 import com.example.testfls.di.utils.injectViewModel
 import com.example.testfls.viewmodel.MainViewModel
 import com.example.testfls.viewmodel.NewsDescriptionViewModel
@@ -24,6 +27,8 @@ class NewsDescriptionFragment : DaggerFragment() {
     lateinit var newsDescriptionViewModel: NewsDescriptionViewModel
     lateinit var mainViewModel: MainViewModel
 
+    lateinit var binding: FragmentNewsDescriptionBinding
+
 
     private fun loadData() {
         newsDescriptionViewModel.sendNewsItem()
@@ -41,48 +46,52 @@ class NewsDescriptionFragment : DaggerFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_news_description, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_news_description, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = this
+
         loadData()
 
         with(newsDescriptionViewModel) {
 
             getNewsItem().observe(viewLifecycleOwner, Observer {
                 if (it != null) {
-                    errorView.visibility = View.GONE
-                    contentView.visibility = View.VISIBLE
+//                    errorView.visibility = View.GONE
+//                    contentView.visibility = View.VISIBLE
                     //         val webSet = full_new.settings
 //        webSet.javaScriptEnabled = true
 //        webSet.allowFileAccess =true
 //        webSet.loadsImagesAutomatically = true
-                    full_new.scrollBarStyle = View.SCROLLBARS_OUTSIDE_OVERLAY
-                    full_new.webViewClient = WebViewClient()
-                    full_new.loadUrl(it.link)
+                    binding.fullNew.scrollBarStyle = View.SCROLLBARS_OUTSIDE_OVERLAY
+                    binding.fullNew.webChromeClient = WebChromeClient()
+                    binding.fullNew.loadUrl(it.link)
 
                     toolbar.title = it.title
                 }
             })
 
 
-            isLoading().observe(viewLifecycleOwner, Observer {
-                if (it != null) {
-                    if (it == true) {
-                        loadingView.visibility = View.VISIBLE
-                    } else loadingView.visibility = View.GONE
-                }
-            })
+//            isLoading().observe(viewLifecycleOwner, Observer {
+//                if (it != null) {
+//                    if (it == true) {
+//                        loadingView.visibility = View.VISIBLE
+//                    } else loadingView.visibility = View.GONE
+//                }
+//            })
 
-            getError().observe(viewLifecycleOwner, Observer {
-                if (it != null) {
-                    contentView.visibility = View.GONE
-                    errorView.visibility = View.VISIBLE
-                    errorView.text =
-                        resources.getText(R.string.description_error_message).toString() + " $it"
-                }
-            })
+//            getError().observe(viewLifecycleOwner, Observer {
+//                if (it != null) {
+//                    contentView.visibility = View.GONE
+//                    errorView.visibility = View.VISIBLE
+//                    errorView.text =
+//                        resources.getText(R.string.description_error_message).toString() + " $it"
+//                }
+//            })
         }
 
     }
